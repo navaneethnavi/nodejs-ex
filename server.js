b744156c375e2ad3cc2852dd4018d90994403cae
navaneethnavi <navaneethnavi@yahoo.co.in>
2018-02-14 10:10:48 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,92 +1,108 @@
-var http = require('http');
-var querystring = require('querystring');
-const crypto = require('crypto');
-var devid='';																						//Device id : Which is send from the board
-var validDeviceId = ['RPI1', 'RPI2', 'RPI3', 'RPI4', 'RPI5', 'RPI6'];								// List of Valid device IDs 
-var dataStore = new  Object();
-  
-http.createServer(function(request, response)
-{
-    request.on('data', function (data)										
-        {			
-            jsonParsed = JSON.parse(data); 															//Json parsed data recieved from the client
-            devid = jsonParsed.DevId; 
-            console.log('\nThis data is recieved from Device : '+"'"+devid+"'"+', Recieved Packet Number : '+jsonParsed.PacketNo);       
-            if(validDeviceId.indexOf(jsonParsed.DevId) != -1)										//returns  the index if match is found returns -1 if match is not found
-			{	
-				if(dataStore[devid]== undefined || dataStore[devid].data1=='')
-				{
-					dataStore[devid ]= new Object();												//creates a new object only once when data is rceved fro a new device.			
-					dataStore[devid].data1 = jsonParsed.Sdata+jsonParsed.TimeS+jsonParsed.RSSI;
-					dataStore[devid].dhash1=jsonParsed.SHA256Hash;
-					console.log('\nData-1: '+dataStore[devid].data1);
-					response.writeHead(200, { 'Content-Type': 'text/plain'});
-					response.end('Hello Node.js Server has recived the data')
-					//response.end();
-				}
-				else
-				{
-					//lastvalidpacket=dataStore[devid].data1;
-					dataStore[devid].data2 = jsonParsed.Sdata+jsonParsed.TimeS+jsonParsed.RSSI;
-					dataStore[devid].dhash2=jsonParsed.SHA256Hash;
-					
-					console.log('\nData1: '+dataStore[devid].data1);
-					console.log('Data2: '+dataStore[devid].data2);
-					
-					dataStore[devid].concdata = dataStore[devid].data1 + dataStore[devid].data2;					
-					
-					dataStore[devid].Servhash=crypto.createHash('sha256').update(dataStore[devid].concdata).digest('hex');
-					
-					console.log('Composed-Data : '+dataStore[devid].concdata);
-					console.log('\nServer Hash : '+dataStore[devid].Servhash);
-					console.log('Client Hash : '+dataStore[devid].dhash1);
-					if(dataStore[devid].Servhash==dataStore[devid].dhash1)
-					{
-						console.log('\n\t\tData Validated');
-						dataStore[devid].dhash1=dataStore[devid].dhash2;
-						dataStore[devid].data1=dataStore[devid].data2;
-					}
-					else
-					{
-						//dataStore[devid].data2=lastvalidpacket;
-						console.log('\n\t      ----------------------------------------------------');
-						console.log('\t******Invalid Packet :Block did not get added to the Chain******');
-						console.log('\t      ----------------------------------------------------');
-					}
-					response.setHeader('Content-Type', 'text/html');
-					response.writeHead(200, { 'Content-Type': 'text/plain'});
-					response.end('Server has received the message');
-					
-				/*	response.writeHead(200, { 'Content-Type': 'text/plain','Trailer': 'Server-Message' });
-					response.addTrailers({ 'Server-Message': 'Ok' });
-					response.end();  */
-				}
-			} 					
-			else
-			{
-				console.log('\n\t#####################################################');
-				console.log('\t# Data Recieved  from an Invalid Device ID -> '+ '"'+devid+'"'+' #');
-				console.log('\t#####################################################');		
-				
-				response.setHeader('Content-Type', 'text/html');
-				response.writeHead(200, { 'Content-Type': 'text/plain'});
-				response.end('Invalid Device  ID |Administrator will be notified');
-				
-				
-				/*const body = 'hello world';
-				response.writeHead(200, body ,{'Content-Length': Buffer.byteLength(body),'Content-Type': 'text/plain' });
+//  OpenShift sample Node application
+var express = require('express'),
+    app     = express(),
+    morgan  = require('morgan');
+    
+Object.assign=require('object-assign')
 
-				
-				var message = 'Invalid Device ID';
-				response.writeHead(400, message, {'content-type' : 'text/plain'});
-				response.end(message);   
-				
-				response.writeHead(400, { 'Content-Type': 'text/plain','Trailer': 'Server-Message' });
-                response.addTrailers({ 'Server-Message': 'Invalid Device ID' });
-				response.end();	*/
-			}
-							 
-        });
-  
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
 
-}).listen(5555);
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
+var db = null,
+    dbDetails = new Object();
+
+var initDb = function(callback) {
+  if (mongoURL == null) return;
+
+  var mongodb = require('mongodb');
+  if (mongodb == null) return;
+
+  mongodb.connect(mongoURL, function(err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = 'MongoDB';
+
+    console.log('Connected to MongoDB at: %s', mongoURL);
+  });
+};
+
+app.get('/', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
+});
+
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
+});
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+initDb(function(err){
+  console.log('Error connecting to Mongo. Message:\n'+err);
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
